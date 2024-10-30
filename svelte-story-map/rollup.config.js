a644338a87c605656dc5228f631dbce2e240e587
Tom Weatherburn <tweatherburn@gmail.com>
2024-10-29 23:38:49 -0400

    minor

---
+++ svelte-story-map/rollup.config.js
@@ -0,0 +1,74 @@
+import { spawn } from 'child_process';
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
+
+export default {
+	input: 'src/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/build/bundle.js'
+	},
+	plugins: [
+		svelte({
+			compilerOptions: {
+				dev: !production
+			}
+		}),
+		css({ output: 'bundle.css' }),
+		resolve({
+			browser: true,
+			dedupe: ['svelte'],
+			exportConditions: ['svelte']
+		}),
+		commonjs(),
+
+		// Serve static files from `static` folder
+		!production &&
+			serve({
+				contentBase: ['public', 'static'], // Add 'static' here
+				port: 5000,
+			}),
+
+		!production &&
+			livereload({
+				watch: 'public',
+				// Include `static` directory for live-reloading
+				verbose: true,
+				contentBase: ['public', 'static']
+			}),
+
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
---
+++ svelte-story-map/src/components/Map.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { onMount } from 'svelte';
+    import maplibre from 'maplibre-gl';
+    import { createEventDispatcher } from 'svelte';
+  
+    let map;
+
+    // Define a function to fly to the passed coordinates and zoom level
+    export const flyTo = (coordinates = [-79.3832, 43.6532], zoomLevel = 14) => {
+        if (map) {
+        map.flyTo({
+            center: coordinates,
+            zoom: zoomLevel,
+            speed: 0.8,
+            curve: 1
+        });
+        }
+    };
+  
+    onMount(() => {
+      map = new maplibre.Map({
+        container: 'map',
+        style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+        center: [-79.3832, 43.6532], // Toronto, for example
+        zoom: 12
+      });
+    });
+  </script>
+  
+  <style>
+    #map {
+      height: 100vh;
+      width: 100%;
+    }
+  </style>
+  
+  <div id="map"></div>
+  
